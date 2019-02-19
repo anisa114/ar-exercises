27e3fe36a2c0d7d851b40181b34cfa21f4f75ee2
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-02-19 11:32:20 -0500

    completed exercise one and created 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,17 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+
+class Store <  ActiveRecord::Base
+  
+end
+
+burnbay = Store.new
+richmond = Store.new
+gastown = Store.new
+
+burnbay = Store.create(name: "Burnbay", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
