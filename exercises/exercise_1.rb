9556f7df7c37ddef06eafbd63d7cfe4d98c1d57a
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-02-19 11:34:41 -0500

    changed variable names into store_1 and such

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,16 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 
-class Store <  ActiveRecord::Base
-  
+class Store <  ActiveRecord::Base 
 end
 
-burnbay = Store.new
-richmond = Store.new
-gastown = Store.new
+store_1 = Store.new
+store_2 = Store.new
+store_3 = Store.new
 
-burnbay = Store.create(name: "Burnbay", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+store_1 = Store.create(name: "Burnbay", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store_2  = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store_3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
