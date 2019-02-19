74e5afa3d12a8110dc4d2a9925e2f5ee93200215
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-02-19 12:51:56 -0500

    Ex.5 calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+@total_revenue= Store.sum("annual_revenue")
+@total_store_count = Store.count
+@average_annual = @total_revenue / @total_store_count
+
+
+puts Store.where("annual_revenue >= ?",1000000).count
