302ee31860d32d2a3a9e7a969767efe8b0122918
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-02-19 12:12:26 -0500

    ex4 loaded a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,24 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store_4 = Store.new
+store_5 = Store.new
+store_6 = Store.new
+
+store_4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store_5  = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store_6 = Store.create(name: "Gastown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores= Store.where(mens_apparel: true)
+
+@mens_stores.find_each do |store|
+   puts store.name
+   puts store.annual_revenue
+end
+
+@womens_stores= Store.where(womens_apparel: true)
+
+@womens_stores.where("annual_revenue < ?",1000000).find_each do |store|
+   puts store.name
+   puts store.annual_revenue
+end
